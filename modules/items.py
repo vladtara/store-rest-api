bc164346caf02e8151e159789e49a543f6e99875
vladyslav.tarasov <vald@glaps.fun>
2022-06-05 20:51:33 +0300

    format

--- modules/items.py
+++ modules/items.py
@@ -15,7 +15,12 @@ class ItemModule(db.Model):
         self.store_id = store_id
 
     def json(self):
-        return {'name': self.name, 'price': self.price, 'store_id': self.store_id}
+        return {
+            'id': self.id,
+            'name': self.name,
+            'price': self.price,
+            'store_id': self.store_id
+        }
 
     @classmethod
     def find_by_name(cls, name):
