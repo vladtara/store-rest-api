7b10f1e568d94bab85fd432c761005de31b5cdab
Tarasov Vladyslav <vlad@glaps.fun>
2022-06-15 10:47:30 +0300

    add __table_args__

--- modules/items.py
+++ modules/items.py
@@ -3,6 +3,7 @@ from db import db
 
 class ItemModule(db.Model):
     __tablename__ = "items"
+    __table_args__ = {'extend_existing': True}
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80))
